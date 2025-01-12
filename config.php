be5886353fbe1db302598a19896cd28a02a1a7cf
BivanS4tr10 <if23.bivansatrio@mhs.ubpkarawang.ac.id>
2025-01-12 23:01:49 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php 
+
+if(!defined('BASEPATH')) echo "Tidak bisa langsung mengakses halaman ini!";
+
+define('DEFAULT_CONTROLLER', 'C_Auth');
+define('BASE_URL', 'https://eloratrans.my.id/');
+define('APP_NAME', 'Aplikasi Rental Mobil');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'eloratra_root');
+define('DB_PASS', 'rootdata13');
+define('DB_NAME', 'eloratra_rental_mobil');
+
+?>
