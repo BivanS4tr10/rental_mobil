539bab78f979023d46e9953212cf9d745752c8a3
dikiraha <kiiinugraha123@gmail.com>
2025-01-13 12:48:30 +0700

    controller jenis bayar

--- config.php
+++ config.php
@@ -3,16 +3,16 @@
 if (!defined('BASEPATH')) echo "Tidak bisa langsung mengakses halaman ini!";
 
 define('DEFAULT_CONTROLLER', 'C_Auth');
-define('BASE_URL', 'https://eloratrans.my.id/');
-// define('BASE_URL', 'http://localhost/rental_mobil/');
+// define('BASE_URL', 'https://eloratrans.my.id/');
+define('BASE_URL', 'http://localhost/rental_mobil/');
 define('APP_NAME', 'Aplikasi Rental Mobil');
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'eloratra_root');
-define('DB_PASS', 'rootdata13');
-define('DB_NAME', 'eloratra_rental_mobil');
-
 // define('DB_HOST', 'localhost');
-// define('DB_USER', 'root');
-// define('DB_PASS', '');
-// define('DB_NAME', 'rental_mobil');
+// define('DB_USER', 'eloratra_root');
+// define('DB_PASS', 'rootdata13');
+// define('DB_NAME', 'eloratra_rental_mobil');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'rental_mobil');
